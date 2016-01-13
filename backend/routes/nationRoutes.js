3ebab4fb211a700366f29ba443b38ffdaa5553b1
Phillip Adamson <adamson.phillip2@gmail.com>
2016-01-13 16:09:29 +0000

    This adds the nation route and model.

---
+++ backend/routes/nationRoutes.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var nationRouter = express.Router();
+var mongoose = require('mongoose');
+var Nation = require('../models/nation');
+
+nationRouter.route('/')
+  .get(function(req, res) {
+    Nation.find({
+      user: req.user
+    }, function(err, nations) {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.send(nations);
+    });
+  })
+  .post(function(req, res) {
+    var nation = new Nation(req.body);
+    console.log(req.user);
+    nation.user = req.user._doc;
+    nation.save(function(err) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(nation);
+      }
+    });
+  });
+
+
+nationRouter.route('/:nationId')
+  .get(function(req, res) {
+    Nation.findOne({
+      id: req.params.nationId,
+      user: req.user
+    }, function(err, nation) {
+      if (err) res.status(500).send(err);
+      res.send(nation);
+    });
+  })
+  .put(function(req, res) {
+    Nation.findOneAndUpdate({
+      id: req.params.nationId,
+      user: req.user
+    }, req.body, {
+      new: true
+    }, function(err, nation) {
+      if (err) res.status(500).send(err);
+      res.send(nation);
+    })
+  })
+  .delete(function(req, res) {
+    Nation.findOneAndRemove({
+      id: req.params.nationId,
+      user: req.user
+    }, function(err, nation) {
+      if (err) res.status(500).send(err);
+      res.send(nation);
+    })
+  });
+
+module.exports = nationRouter;
