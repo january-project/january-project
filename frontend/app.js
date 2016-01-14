0280d48d03dc4091c3103eb6675f7c064ddf9c2b
Phillip Adamson <adamson.phillip2@gmail.com>
2016-01-14 18:34:19 +0000

    This adds the functionality where the navbar displays links based on authentication

--- frontend/app.js
+++ frontend/app.js
@@ -1,21 +1,20 @@
 var app = angular.module("NSApp", ["ngRoute", "NSAuth"]);
 
 app.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: "home/home.html",
-        })
-        .when('/signup', {
-            templateUrl: 'auth/signup.html',
-            controller: 'SignupController'
-        })
-        .when('/login', {
-            templateUrl: 'auth/login.html',
-            controller: 'LoginController'
-        })
-		.when('/logout', {
-			templateUrl: 'auth/logout.html',
-			controller: 'LogoutController'
-		
-		});
+  $routeProvider
+    .when('/home', {
+      templateUrl: "home/home.html",
+    })
+    .when('/signup', {
+      templateUrl: 'auth/signup.html',
+      controller: 'SignupController'
+    })
+    .when('/login', {
+      templateUrl: 'auth/login.html',
+      controller: 'LoginController'
+    })
+    .when('/logout', {
+      template: '',
+      controller: 'LogoutController'
+    })
 });
